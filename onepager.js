6d099c243176a29fa5d150fd8bfc981df81f547d
Mac <mac@Macs-MacBook-Pro.local>
2022-08-29 13:54:52 +0400

    added if statement for definition mobile with  desktop version

--- onepager.js
+++ onepager.js
@@ -3,17 +3,54 @@
 const sliderLeft = document.querySelector(".arrowLeft");
 const sliderRight = document.querySelector(".arrowRight");
 let imageSlide = document.querySelector(".imageCont");
+let slidePhoto = document.querySelector(".slide1");
+let photoStyle = getComputedStyle(slidePhoto);
 
-console.log("sliderLeft");
+
+
 
 sliderLeft.addEventListener("click", function(event){
 
-    imageSlide.scrollLeft+= 570;
+    console.log(slidePhoto.clientWidth);
+
+    if(slidePhoto.clientWidth < 300){
+
+        imageSlide.scrollLeft+= 285;
+
+        console.log(imageSlide);
+    }
+    else{
+        imageSlide.scrollLeft+= 570;
+        console.log(sliderLeft);
+    }
+
+    
+
+  
+    
+    
 
 })
 
 sliderRight.addEventListener("click", function(event){
 
-    imageSlide.scrollLeft-= 570;
+    if(slidePhoto.clientWidth < 300){
+
+        imageSlide.scrollLeft-= 285;
+
+        console.log(imageSlide);
+    }
+    else{
+        imageSlide.scrollLeft-= 570;
+        console.log(sliderLeft);
+    }
+
+   
+
+     
+    
+
+   
 
 })
+
